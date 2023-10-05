3b3736f3bbbee4a58928e1f5f696c782b8b57c44
Bakr Ibrahimi <bakribra5@gmail.com>
2023-10-06 00:02:04 +0100

    add task1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Fabric script that generates a tgz archive from the contents of the web_static
+folder of the AirBnB Clone repo
+"""
+
+from datetime import datetime
+from fabric.api import local
+from os.path import isdir
+
+
+def do_pack():
